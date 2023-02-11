c3d5219737436d2c1406ff8d9cad6f822ba12822
MASTER_SABBIR <sabbir117824@gmail.com>
2023-02-12 03:19:53 +0600

    sabbir

---
+++ file.go
@@ -0,0 +1,10 @@
+package main 
+import "fmt"
+func main(){
+	var name string= "Sabbir Hossan"
+	var age int= 23
+	var collage string= "open university"
+	var gpa float32= 3.78
+	var city string= "Chittagong,Bangladesh]"
+	fmt.Println(name,age,collage,gpa,city)
+}
