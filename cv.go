717338dfd7a6aff4065cb199359de0189af63acf
MASTER_SABBIR <sabbir117824@gmail.com>
2023-02-14 02:11:59 +0600

    initial commit

---
+++ cv.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "SABBIR HOSSAN"
+	var father string = "JAHANGIR ALAM"
+	var mother string = "SALMA BEGUM"
+	var area string = "East nasirbad,baby super market"
+	var city string = "Chittagong"
+	var country string = "Bangladesh"
+	var age int = 23
+	fmt.Println(name,father,mother,area,city,country,age)
+}
