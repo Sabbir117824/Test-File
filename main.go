6ec103c135f294e0dfac539ca59b2b4e0f72795f
MASTER_SABBIR <sabbir117824@gmail.com>
2023-02-17 00:42:39 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+fmt.Println("Enter Your name & age:")
+var name string
+var age int
+
+fmt.Scanf("%s %d", &name, &age)
+
+fmt.Printf("Your name %s , age is %d", name , age)
+
+
+
+
+
+}
